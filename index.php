54e58e08c90af3bde9aec3f7abef47dd1e89a06c
Alexander Lurita <alex11lurita@gmail.com>
2023-05-23 17:54:28 -0500

    Index indicaciones

--- index.php
+++ index.php
@@ -9,8 +9,14 @@
   <!-- Bootstrap 5.3 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
+  <link rel="stylesheet" href="./styles/index.css">
 </head>
 <body>
-  <a class="btn btn-success rounded" href="./login.php">Login</a>
+  <div class="container mt-3">
+      <h2>Pantalla principal</h2>
+      <h3>Carta</h3>
+      <p>Los usuarios iniciarán sesión aquí (Empleados por ahora)</p>
+      <a class="btn btn-success rounded" href="./login.php">Login</a>
+  </div>
 </body>
 </html>
