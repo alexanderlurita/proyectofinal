7ec099f0360d4b7951bd0bf95994bb0b0c64bc7d
Alexander Lurita <alex11lurita@gmail.com>
2023-05-25 12:09:18 -0500

    Login actualizado, incorporando nuevo logo

--- login.php
+++ login.php
@@ -23,48 +23,47 @@ if(isset($_SESSION['seguridad']) && $_SESSION['seguridad']['login']) {
   <!-- Container -->
   <div class="container d-flex justify-content-center align-items-center min-vh-100"> 
       <div class="row border rounded-5 p-3 bg-white shadow box-area">
-        <!-- Logo -->
-        <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box bg-primary"">
+        <!-- Contenedor del LOGO -->
+        <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box">
           <div class="featured-image">
-            <img src="./images/logo.png" class="img-fluid" style="width: 225px;">
-           </div>
-           <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600">Be Verified</p>
-           <small class="text-white text-wrap text-center" style="width: 17rem; font-family: 'Courier New', Courier, monospace;">Join experienced Designers on this platform.</small>
+            <img id="logo" src="./images/mil_sabores_logo_blanco.png" class="img-fluid" style="width: 250px;">
+          </div>
+          <small class="text-white text-wrap text-center" style="width: 17rem; font-family: 'Courier New', Courier, monospace;">Sabores exquisitos que deleitan tus sentidos.</small>
         </div> <!-- Fin logo -->
         <!-- Login -->
         <div class="col-md-6 right-box">
           <div class="row align-items-center">
             <div class="header-text mb-4">
               <h2>¡Bienvenido!</h2>
-              <p>Inicia sesión con tu cuenta para continuar.</p>
+              <p>Ingresa los datos de tu cuenta para continuar.</p>
             </div>
             <!-- Form input-groups -->
             <form action="" autocomplete="off">
               <div class="input-group mb-3">
-                <input type="text" id="username" class="form-control form-control-lg bg-light fs-6" placeholder="Username" autofocus>
+                <input type="text" id="username" class="form-control form-control-lg bg-light fs-6" placeholder="Nombre de usuario" autofocus>
               </div>
               <div class="input-group mb-1">
-                <input type="password" id="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                <input type="password" id="password" class="form-control form-control-lg bg-light fs-6" placeholder="Contraseña">
               </div>
               <div class="input-group mb-5 d-flex justify-content-between">
                 <div class="form-check">
                   <input type="checkbox" class="form-check-input" id="formCheck">
-                  <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
+                  <label for="formCheck" class="form-check-label text-secondary"><small>Recuérdame</small></label>
                 </div>
                 <div class="forgot">
-                  <small><a href="#">Forgot Password?</a></small>
+                  <small><a href="#">¿Olvista tu contraseña?</a></small>
                 </div>
               </div>
             </form>
             <!-- Buttons -->
             <div class="input-group mb-3">
-              <button id="login" class="btn btn-lg btn-primary w-100 fs-6" type="button">Login</button>
+              <button id="login" class="btn btn-lg text-light w-100 fs-6" type="button">Iniciar sesión</button>
             </div>
             <div class="input-group mb-3">
               <button class="btn btn-lg btn-light w-100 fs-6" type="button"><img src="./images/google.png" style="width: 20px;" class="me-2"><small>Sign In with Google</small></button>
             </div>
             <div class="row">
-              <small>Don't have account? <a href="#">Sign Up</a></small>
+              <small>¿No tienes una cuenta? <a href="#">Registrarse</a></small>
             </div>
 
           </div>
